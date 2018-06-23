65579b3fba5a317ae87b23c44c409638e743b8e0
Derek Stinson <dereklstinson@gmail.com>
2018-06-23 17:51:22 -0400

    middle of building

--- testing/mnist/model.go
+++ testing/mnist/model.go
@@ -37,44 +37,49 @@ func (net *network) Append(input interface{}) {
 		net.model = append(net.model, sect)
 	}
 }
-
+func errcheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 func model() network {
 
-	shape := gocudnn.Shape
+	shape := gocudnn.Shape //simple arguments to array maker
 	var flg gocudnn.Flags
 	Float := flg.DataType.Float()
 	NCHW := flg.TensorFormat.NCHW()
 
 	inputDesc, err := gocudnn.NewTensor4dDescriptor(Float, NCHW, shape(1, 1, 28, 28))
-	if err != nil {
-		panic(err)
-
-	}
+	errcheck(err)
 	size, err := inputDesc.GetSizeInBytes()
-	if err != nil {
-		panic(err)
-	}
-	inputmemHost, err := MakeHostMem(size, Float, 0.0, false)
-	if err != nil {
-		panic(err)
-	}
+	errcheck(err)
+	inputmemHost, err := makehostmem(size, Float, 0.0, false)
+	errcheck(err)
 	HTD := flg.MemcpyKind.HostToDevice()
 	inputmemDevice, err := gocudnn.Malloc(size)
-	if err != nil {
-		panic(err)
-	}
+	errcheck(err)
 	err = gocudnn.CudaMemCopy(inputmemDevice, inputmemHost, size, HTD)
-	if err != nil {
-		panic(err)
-	}
+	errcheck(err)
 
 	inputlayer := layers.BuildIO(inputDesc, inputmemDevice, nil)
 	var networkmod network
+
 	networkmod.Append(inputlayer)
+	/*
+	   Layer 1
+	*/
+
+	crosscorr := flg.ConvolutionMode.CrossCorrelation()
+	convdesc, err := gocudnn.NewConvolution2dDescriptor(crosscorr, Float, shape(2, 2), shape(1, 1), shape(1, 1))
+	ConvPref:=flg.ConvolutionFwdPref.NoWorkSpace()
+	errcheck(err)
+filter1,err:=	gocudnn.NewFilter4dDescriptor(Float,NCHW,shape(10,10,5,5)
+errcheck(err)
+    cnn.LayerSetup(inputDesc,filter1,convdesc,)
 	return networkmod
 }
 
-func MakeHostMem(size gocudnn.SizeT, kind gocudnn.DataType, fanin float64, rand bool) (*gocudnn.GoPointer, error) {
+func makehostmem(size gocudnn.SizeT, kind gocudnn.DataType, fanin float64, rand bool) (*gocudnn.GoPointer, error) {
 	var flg gocudnn.DataTypeFlag
 	switch kind {
 	case flg.Float():
