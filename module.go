92a77b0c90fe104f3b555e3a3af8660cc99ef110
Derek Stinson <dereklstinson@gmail.com>
2018-10-06 12:40:37 -0400

    changes

--- module.go
+++ module.go
@@ -6,20 +6,27 @@ import (
 )
 
 type Module struct {
-	index    int64
-	children []int
-
-	x, y  []*layers.IO
+	index int64
 	layer []*layer
 	mem   []*layers.IO
 }
-type inputs struct {
+type connection struct {
+	sharedid int
+	id       int
 	recieved bool
-	x        []*layer
+	x        *layers.IO
+}
+type concatconnection struct {
+	connection []connection
+}
+type ConcatModule struct {
+	index int64
+	layer []*layer
+}
+type SplitModule struct {
 }
 
 //ForwardProp does the forward prop for a prebuilt module
-//func (m *Module) ForwardLoad()
 func (m *Module) ForwardProp(handle Handles, stream *gocudnn.Stream, wspace *gocudnn.Malloced, x, y *layers.IO) error {
 
 	err := m.layer[0].forwardprop(handle, wspace, x, m.mem[0])
