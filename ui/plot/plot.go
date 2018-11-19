70328c955b3d5734e08292ebc72c340748eb0eb3
Derek <dereklstinson@gmail.com>
2018-11-19 02:45:56 -0500

    got the ui moving in the right direction. Adding some plotting stuff

---
+++ ui/plot/plot.go
@@ -0,0 +1,99 @@
+package plot
+
+import (
+	"errors"
+	"io"
+	"math/rand"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+//LabeledData holdes the label and the data for the plot
+type LabeledData struct {
+	Label string
+	Data  plotter.XYs
+}
+
+//NewLabeledData will maake a labeled data.  data supported is []float32, []float64, and plotter.XYs.
+func NewLabeledData(label string, data interface{}) (LabeledData, error) {
+
+	switch x := data.(type) {
+	case plotter.XYs:
+		return LabeledData{
+			Label: label,
+			Data:  x,
+		}, nil
+	case []float64:
+		pts := make(plotter.XYs, len(x))
+		for i := range x {
+			pts[i].X = float64(i)
+			pts[i].Y = x[i]
+		}
+		return LabeledData{
+			Label: label,
+			Data:  pts,
+		}, nil
+	case []float32:
+		pts := make(plotter.XYs, len(x))
+		for i := range x {
+			pts[i].X = float64(i)
+			pts[i].Y = float64(x[i])
+		}
+		return LabeledData{
+			Label: label,
+			Data:  pts,
+		}, nil
+	}
+	return LabeledData{}, errors.New("Not Supported data")
+}
+
+//Verses does a verses line plot with the data passed.
+//title,xaxis,yaxis are the labels for the plot image
+//h,w are the size of the image
+//returns a WriteTo.  I did this because I thought the user might want to store an array of them
+func Verses(title, xaxis, yaxis string, h, w int, data ...LabeledData) (io.WriterTo, error) {
+	rand.Seed(int64(20))
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = title
+	p.X.Label.Text = xaxis
+	p.Y.Label.Text = yaxis
+	for i := range data {
+		err = plotutil.AddLines(p, data[i].Label, data[i].Data)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return p.WriterTo(vg.Length(h)*vg.Centimeter, vg.Length(w)*vg.Centimeter, "jpg")
+
+}
+
+//Difference will subtract that datapoints of the Y axis of B from A.  It will also add to the plot  "A.Label - B.Label"
+//A and B need to have the same number of elements.  This only works on discrete n arrays so x=i intigers
+//It will return an io.WriterTo and an error
+func Difference(title, xaxis, yaxis string, h, w int, A, B LabeledData) (io.WriterTo, error) {
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = title
+	p.X.Label.Text = xaxis
+	p.Y.Label.Text = yaxis
+	pts := make(plotter.XYs, len(A.Data))
+	for i := range A.Data {
+		pts[i].Y = A.Data[i].Y - B.Data[i].Y
+		pts[i].X = float64(i)
+	}
+	//	plotter.NewHistogram()
+	err = plotutil.AddLines(p, A.Label+"-"+B.Label, pts)
+	return p.WriterTo(vg.Length(h)*vg.Centimeter, vg.Length(w)*vg.Centimeter, "jpg")
+
+}
--- ui/server2.go
+++ ui/server2.go
@@ -17,13 +17,14 @@ const testingnewport = ":8081"
 //ServerMain2 is the main
 func ServerMain2() {
 	// Hello world, the web server
-	url := SetupURL("/loss/", "/inputimage/", "/outputimage/")
+	url := SetupURL("/loss/", "/inputimage/", "/outputimage/", "/extra1/", "/extra2/", "/extra3/")
 	handlers := make([]imageHandler, 0)
 	for i := range url.url {
 		handlers = append(handlers, makeimagehandler(imagefolderlocation, i*20))
 	}
+	serverlocation := "http://localhost" + testingnewport
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		holdertemplate2(w, "nothing", url.url)
+		holdertemplate2(w, serverlocation, url.url)
 
 		//	w.Header().Set("Cache-Control", "no-store")
 
@@ -43,10 +44,14 @@ type URLs struct {
 	url []string
 }
 
+//WebPage is the webpage stuff
 type WebPage struct {
 	Url1 string
 	Url2 string
 	Url3 string
+	Url4 string
+	Url5 string
+	Url6 string
 }
 
 //SetupURL to stream images
@@ -77,7 +82,7 @@ func (h *imageHandler) F() func(w http.ResponseWriter, req *http.Request) {
 		check(err)
 		//	buffer := new(bytes.Buffer)
 		check(jpeg.Encode(w, img, nil))
-		if h.index < len(h.path) {
+		if h.index < len(h.path)-1 {
 			h.index++
 		} else {
 			h.index = 0
@@ -98,11 +103,14 @@ func (h *imageHandler) F() func(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func holdertemplate2(w http.ResponseWriter, pagelocation string, data []string) {
+func holdertemplate2(w http.ResponseWriter, serverlocation string, data []string) {
 	urls := WebPage{
-		Url1: "http://localhost:8081/loss/",        //data[0],
-		Url2: "http://localhost:8081/inputimage/",  //data[1],
-		Url3: "http://localhost:8081/outputimage/", //data[2],
+		Url1: serverlocation + data[0],
+		Url2: serverlocation + data[1],
+		Url3: serverlocation + data[2],
+		Url4: serverlocation + data[3],
+		Url5: serverlocation + data[4],
+		Url6: serverlocation + data[5],
 	}
 	//t, err := template.ParseFiles(pagelocation)
 	t, err := template.New("webpage").Parse(thewebpagetemplate2())
