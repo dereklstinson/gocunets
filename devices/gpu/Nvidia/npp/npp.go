4e494db60481a7ab1f2564fb6bb94f1d63ea521b
Derek Stinson <dereklstinson@gmail.com>
2019-02-25 14:43:22 -0500

    needs work

---
+++ devices/gpu/Nvidia/npp/npp.go
@@ -0,0 +1,26 @@
+package npp
+
+import (
+	"github.com/dereklstinson/GoCudnn/npp"
+	"github.com/dereklstinson/GoCudnn/npp"
+)
+
+
+
+
+
+fucn ResizeSqPixelC1R(src interface{},srcsize npp.Size, srcstep int32, srcROI npp.Rect, dst interface,dststep int32, dstROI npp.Rect, xFactor, yFactor, xShift, yShift,interpol npp.InterpolationMode, channel int32 )error{
+	switch x:=src.(type){
+	case *npp.Uint8:
+
+	case *npp.Uint16:
+
+	case *npp.Int16:
+
+	case *npp.Float32:
+
+	case *npp.Float64:
+
+	}
+	npp.ResizeSqrPixel8uC3R()
+}
